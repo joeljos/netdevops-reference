ff4dba8cc6c890da2b6e808def538dbdb4897de8
Joel Jose <11584709+joeljos@users.noreply.github.com>
2023-07-09 18:19:32 +0530

    update

--- ShutAllInterface/revert_shutRtr1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python noshutinterfaces.py --testbed-file testbed.yaml --devices dist-rtr02 --exclude dist-rtr02:GigabitEthernet1
--- ShutAllInterface/shutRtr1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python shutinterfaces.py --testbed-file testbed.yaml --devices dist-rtr01 --exclude dist-rtr01:GigabitEthernet1
